5fcae3f1866f7258b0c31acc208834bec815eff2
Girish Gupta <girish@girishgupta.com>
2023-10-20 21:48:46 -0700

    Adds timing

--- bvp/src/App.js
+++ bvp/src/App.js
@@ -1,11 +1,57 @@
 import "./App.css";
 import banner from "./banner.png";
 import { useState } from "react";
+import { Menu } from "@headlessui/react";
 import axios from "axios";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+
+  const meetings = [
+    {
+      id: 1,
+      name: "Music from DJ Rasheq",
+      start: "12:00 PM",
+      end: "12:45 PM",
+    },
+    {
+      id: 2,
+      name: "Music from local neighbor Al Hertz",
+      start: "12:45 PM",
+      end: "1:15 PM",
+    },
+    {
+      id: 3,
+      name: "Music from Emmy-winner Tony Saunders",
+      start: "1:15 PM",
+      end: "2:30 PM",
+    },
+    {
+      id: 4,
+      name: "Supervisor Rafael Mandelman",
+      start: "2:30 PM",
+      end: "2:45 PM",
+    },
+    {
+      id: 5,
+      name: "Tai Chi with Usha Gupta",
+      start: "3:00 PM",
+      end: "3:30 PM",
+    },
+    {
+      id: 6,
+      name: "Music from Max Baker and Randall Rattray",
+      start: "3:30 PM",
+      end: "4:30 PM",
+    },
+    {
+      id: 7,
+      name: "Open Talent Show",
+      start: "4:30 PM",
+      end: "5:00 PM",
+    },
+  ];
 
   const [formData, setFormData] = useState({
     name: "",
@@ -64,10 +110,40 @@ function App() {
               potluck and more!
             </p>
             <br />
+            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+              {meetings.map((meeting) => (
+                <li
+                  key={meeting.id}
+                  className="group flex items-center space-x-4 rounded-xl px-4 py-2 focus-within:bg-gray-100 hover:bg-gray-100"
+                >
+
+                  <div className="flex-auto">
+                    <p className="text-gray-900">{meeting.name}</p>
+                    <p className="mt-0.5">
+                      <time dateTime={meeting.startDatetime}>
+                        {meeting.start}
+                      </time>{" "}
+                      -{" "}
+                      <time dateTime={meeting.endDatetime}>{meeting.end}</time>
+                    </p>
+                  </div>
+                  <Menu
+                    as="div"
+                    className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+                  >
+                    <div>
+                      <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+                        <span className="sr-only">Open options</span>
+                      </Menu.Button>
+                    </div>
+                  </Menu>
+                </li>
+              ))}
+            </ol>
+            <br />
             <i>Joining us are:</i>
             <br />
-            Mayor London Breed, Supervisor Rafael Mandelman, State Senator Scott
-            Weiner, Assembly Member Matt Haney, SFPD Captain Jack Hart, and
+            Mayor London Breed, City Attorney David Chiu, SFPD Captain Jack Hart with a fire truck, and many
             others.
           </div>
           <div className="mt-10 flex flex-col items-center justify-center gap-y-6">
